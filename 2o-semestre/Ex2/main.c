f602b7b73ef2a5ca74041680c19ab95cc2d7aebe
João Vítor Cruz <163170095+joao-vitor-cruz@users.noreply.github.com>
2024-09-02 13:36:11 -0300

    Inclusão ex2

--- 2o-semestre/Ex2/main.c
+++ 2o-semestre/Ex2/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <locale.h>
 #define ex1
+
+#ifdef ex1
 /*1 - Escreva um programa para adivinhar um numero entre 1 e 99 que o usuario
     pensou. Digite via teclado os simbolos =, > ou < a cada pergunta. Utilize o
     comando if-else.*/
-#ifdef ex1
+
 int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8"); 
     
@@ -37,3 +39,50 @@ int main() {
 	}
 #endif
 
+#ifdef ex2
+/*2 - Reescreva o programa do exercicio anterior agora utilizando o comando switch.
+    Conte o n. de tentativas e imprima o resultado no video.*/
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8"); 
+    
+	int x = 50;
+	int min = 1;
+	int max = 100;
+	char resposta;
+
+	printf("Irei adivinhar o número que você pensar. Um número será mostrado a seguir, escreva >, < ou = para indicar o quão próximo o número mostrado está do seu.\n");
+
+	while (1) {
+		printf("O número é %d.\n", x);
+		scanf(" %c", &resposta);
+        
+        switch (resposta) {
+            case '>': 
+            {
+                min = x + 1;
+			    x = (x + max) / 2;
+			    break;
+            }
+            
+            case '<':
+            {
+                max = x - 1;
+                x = (min + x)/2;
+                break;
+            }
+            
+            case '=':
+            {
+                printf("Adivinhei!! O número é %d!\n", x);
+                return 0;
+            }
+            
+            default:
+            {
+                printf("Valor inválido, digite novamente");
+            }
+        }
+	}
+}
+#endif
