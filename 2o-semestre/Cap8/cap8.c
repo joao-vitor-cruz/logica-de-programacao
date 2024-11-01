303abe2bace80ad008556f30534434fb6d2ad6ad
João Vítor Cruz <163170095+joao-vitor-cruz@users.noreply.github.com>
2024-11-01 20:25:09 -0300

    Create cap8.c

---
+++ 2o-semestre/Cap8/cap8.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define ex1
+
+#ifdef ex1
+/*1 - Escreva um programa que receba via teclado usando ponteiros um char,
+    int, long, unsigned, float, double, unsigned long e unsigned char,
+    e imprima-os no video utilizando ponteiros no seguinte formato:
+
+            10        20        30        40        50        60
+    123456789012345678901234567890123456789012345678901234567890
+        int                 long                unsigned
+                  float               double              char
+             unsigned long       unsigned char
+*/
+
+#endif
+
+#ifdef ex2
+/*2 - Receba 2 string de ate 10 caracteres via teclado na funcao main(). Faça uma
+    funcao para compara-las usando ponteiros e retorne como resultado se 
+    sao IGUAIS 1 ou se sao DIFERENTES 0 para a funcao main(). 
+    Imprima o resultado na funcao main(). Declare os vetores como variavel global
+*/
+
+#endif
+
+#ifdef ex3
+/*3 - Escreva um programa que receba uma letra via teclado usando ponteiro. Escreva 
+    uma funcao que pesquise se esta letra existe no vetor abaixo usando ponteiros. 
+    Imprima o resultado da pesquisa no video na funcao main(). Passe como informacao
+    para a funcao a letra digitada usando ponteiros.(utilize o comando return). 
+    O vetor deve ser declarado como variavel global.
+
+    vetor -> b,d,f,h,j,k,m,o,q,s,u,w,y
+  */
+
+#endif
+
+#ifdef ex4
+/*4 - Escreva um programa que receba via teclado numeros inteiros positivos usando
+    ponteiros. Quando o numero digitado for negativo o programa deve parar e calcula
+    a media dos valores digitados e mostrar o resultado. Todos os calculos devem ser
+    feitos usando ponteiro.
+*/
+
+#endif
+
+#ifdef ex5
+/*5 - Com a estrutura abaixo, defina um vetor de estruturas de 4 elementos.
+    Receba os dados via teclado numa funcao usando ponteiros. Imprima os dados
+    recebidos no video numa usando ponteiros. Utilize um comando de loop. 
+    O vetor de estruturas de ser declarado como variavel global
+       estrutura: nome, end, cidade, estado, cep
+*/
+
+#endif
+
+#ifdef ex6
+/*6 - Escreva um programa com a estrutura abaixo. Receba 2 datas via teclado 
+    usando ponteiros. Calcule e exiba o numero de dias entre as duas datas
+    usando ponteiros. Utilize um vetor de estruturas.
+    	estrutura: dia, mes e ano
+*/
+
+#endif
